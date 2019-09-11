e04abe1f69f63324d28c6a7d196dd31ef9f7632d
H <h@MacBook-Pro-de-H.local>
2019-09-11 09:41:28 -0600

    add links to contact buttons

--- js/hmenu.js
+++ js/hmenu.js
@@ -16,69 +16,67 @@ const wrapper = document.getElementById('menu-icon-wrapper');
 
 wrapper.style.visibility = 'visible';
 
+function inAC(s) {
+  s.draw('80% - 240', '80%', 0.3, {
+    delay: 0.1,
+    callback() {
+      inAC2(s);
+    },
+  });
+}
 
+function inAC2(s) {
+  s.draw('100% - 545', '100% - 305', 0.6, {
+    easing: ease.ease('elastic-out', 1, 0.3),
+  });
+}
+
+function inB(s) {
+  s.draw(beginB - 60, endB + 60, 0.1, {
+    callback() {
+      inB2(s);
+    },
+  });
+}
+
+function inB2(s) {
+  s.draw(beginB + 120, endB - 120, 0.3, {
+    easing: ease.ease('bounce-out', 1, 0.3),
+  });
+}
+
+/* Out animations (to burger icon) */
+
+function outAC(s) {
+  s.draw('90% - 240', '90%', 0.1, {
+    easing: ease.ease('elastic-in', 1, 0.3),
+    callback() {
+      outAC2(s);
+    },
+  });
+}
+
+function outAC2(s) {
+  s.draw('20% - 240', '20%', 0.3, {
+    callback() {
+      outAC3(s);
+    },
+  });
+}
+
+function outAC3(s) {
+  s.draw(beginAC, endAC, 0.7, {
+    easing: ease.ease('elastic-out', 1, 0.3),
+  });
+}
+
+function outB(s) {
+  s.draw(beginB, endB, 0.7, {
+    delay: 0.1,
+    easing: ease.ease('elastic-out', 2, 0.4),
+  });
+}
 (function () {
-  /* In animations (to close icon) */
-  function inAC(s) {
-    s.draw('80% - 240', '80%', 0.3, {
-      delay: 0.1,
-      callback() {
-        inAC2(s);
-      },
-    });
-  }
-
-  function inAC2(s) {
-    s.draw('100% - 545', '100% - 305', 0.6, {
-      easing: ease.ease('elastic-out', 1, 0.3),
-    });
-  }
-
-  function inB(s) {
-    s.draw(beginB - 60, endB + 60, 0.1, {
-      callback() {
-        inB2(s);
-      },
-    });
-  }
-
-  function inB2(s) {
-    s.draw(beginB + 120, endB - 120, 0.3, {
-      easing: ease.ease('bounce-out', 1, 0.3),
-    });
-  }
-
-  /* Out animations (to burger icon) */
-
-  function outAC(s) {
-    s.draw('90% - 240', '90%', 0.1, {
-      easing: ease.ease('elastic-in', 1, 0.3),
-      callback() {
-        outAC2(s);
-      },
-    });
-  }
-
-  function outAC2(s) {
-    s.draw('20% - 240', '20%', 0.3, {
-      callback() {
-        outAC3(s);
-      },
-    });
-  }
-
-  function outAC3(s) {
-    s.draw(beginAC, endAC, 0.7, {
-      easing: ease.ease('elastic-out', 1, 0.3),
-    });
-  }
-
-  function outB(s) {
-    s.draw(beginB, endB, 0.7, {
-      delay: 0.1,
-      easing: ease.ease('elastic-out', 2, 0.4),
-    });
-  }
   trigger.onclick = function () {
     if (toCloseIcon) {
       inAC(segmentA);
